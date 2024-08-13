f3b68c722d1641b37b8a50fe521d468f19a4167f
sjz <13105478410@163.com>
2024-08-13 23:06:47 +0800

    123
    123

---
+++ R/RCDS.R
@@ -0,0 +1,44 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+
+RCDS=function(data){
+  CalRiskScore <- function(fit, new_data, type = "lp"){
+    new_data <- new_data[, fit$subFeature]
+    RS <- quiet(switch(
+      EXPR = class(fit)[1],
+      "coxnet"      = predict(fit, type = 'link', as.matrix(new_data)),
+      "coxph"       = predict(fit, type = 'lp', as.data.frame(new_data)),
+      "survivalsvm" = predict(fit, as.data.frame(new_data))$predicted,
+      "CoxBoost"    = predict(fit, type = "lp", as.data.frame(new_data)),
+      "superpc"     = superpc.predict(object = fit,
+                                      data = fit$data,
+                                      newdata = list(x = t(scale(as.matrix(new_data)))),
+                                      threshold = fit$threshold,
+                                      n.components = 1)$v.pred,
+      "plsRcoxmodel" = predict(fit, type = "lp", as.data.frame(new_data)),
+      "rfsrc"        = predict(fit, as.data.frame(new_data))$predicted,
+      "gbm"          = predict(fit, type = 'link', as.data.frame(new_data))
+    ))
+    RS = as.vector(RS)
+    names(RS) = rownames(new_data)
+    return(RS)
+  }
+  int=intersect(colnames(data),fit$subFeature)
+  data=data[,fit$subFeature]
+  data=scale(data)
+  score<- CalRiskScore(fit =fit, new_data =data,type = "lp")
+}
+
